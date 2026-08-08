4fed19949036d58990d3a531e08f4832b3df857a
VarshaChukka <2303A52494@sru.edu.in>
2026-08-08 09:40:10 +0530

    Initial commit

---
+++ src/Task.java
@@ -0,0 +1,37 @@
+public class Task {
+
+    private int taskId;
+    private String taskName;
+    private boolean completed;
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Task ID : " + taskId +
+                "\nTask : " + taskName +
+                "\nCompleted : " + completed;
+    }
+}
