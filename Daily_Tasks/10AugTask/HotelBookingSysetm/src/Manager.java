8b6d2feadffd293ed8ae24bf80b089ac468cc3ea
VarshaChukka <2303A52494@sru.edu.in>
2026-08-21 07:55:47 +0530

    Add files via upload

---
+++ Daily_Tasks/10AugTask/HotelBookingSysetm/src/Manager.java
@@ -0,0 +1,10 @@
+public class Manager extends Person {
+
+    public void manageStaff() {
+        System.out.println("Managing staff...");
+    }
+
+    public void recordComplaints() {
+        System.out.println("Recording complaint...");
+    }
+}
---
+++ Daily_Tasks/10AugTask/HotelBookingSysetm/src/Staff.java
@@ -0,0 +1,22 @@
+public class Staff extends Person {
+
+    public void editRoom() {
+        System.out.println("Editing room...");
+    }
+
+    public void makeDiscount() {
+        System.out.println("Making discount...");
+    }
+
+    public void generateReport() {
+        System.out.println("Generating report...");
+    }
+
+    public void bookRoom() {
+        System.out.println("Booking room...");
+    }
+
+    public void generateBill() {
+        System.out.println("Generating bill...");
+    }
+}
